2054006cc1bda0d57885947832bf214a07d96d70
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2018-12-24 17:58:26 +0600

    modified CardDeck file and fix bugs

--- src/components/CardDeck/CardDeck.js
+++ src/components/CardDeck/CardDeck.js
@@ -43,38 +43,93 @@ class CardDeck extends Component {
         this.setState({currentCards, text: ''});
     };
 
+    getUniqueValues = (array) => {
+        return [...new Set(array)];
+    };
+
+    countSimilars = (array) => {
+        const obj = {};
+        for (let i = 0; i < array.length; i++) {
+            const currentValue = array[i];
+            if (obj[currentValue]) {
+                obj[currentValue]++
+            } else {
+                obj[currentValue] = 1
+            }
+        }
+        return obj
+    };
+
     getOutcome = () => {
-        let part = [...this.state.part];
-        let number = 0;
-        let suit1 = part[0].suit;
-        if (suit1 === part[1].suit && suit1 === part[2].suit && suit1 === part[3].suit && suit1 === part[4].suit) {
-            number = -1;
-        }
-        for (let j = 0; j < 5; j++) {
-            let value = part[j].rank;
-            let suit = part[j].suit;
+        let currentCards = [...this.state.currentCards];
+        currentCards.sort((a, b) => a.order - b.order).reverse();
+        const suits = [];
+        const ranks = [];
+        const orders = [];
 
-            part.forEach(function (element) {
-                if (value === element.rank && suit !== element.suit) {
-                    number++;
-                }
-            });
+        for (let i = 0; i < currentCards.length; i++) {
+            let card = currentCards[i];
+            suits.push(card.suit);
+            ranks.push(card.rank);
+            orders.push(card.order);
         }
-
-        let text = '';
-        if (number === 2) {
-            text = "Congratulations! One pair";
-        } else if (number === 4) {
-            text = "Well done! Two pairs";
-        } else if (number === 6) {
-            text = "Well done! Three pairs";
-        } else if (number === -1) {
-            text = "Flash!";
-        } else {
-            text = "No pairs found";
+        const uniqueSuits = this.getUniqueValues(suits);
+        const hasSimilarSuits = uniqueSuits.length === 1;
+        const hasRoyalFleshRanks = ranks.includes('10') && ranks.includes('J') && ranks.includes('Q') && ranks.includes('K') && ranks.includes('A')
+        if (hasRoyalFleshRanks && hasSimilarSuits) {
+            this.setState({text: 'Royal flush!!!'});
+            return
         }
 
-        this.setState({text});
+        const differencesArray = orders.map((currentValue, index, array) => {
+            const next = index === array.length - 1 ? currentValue - 1 : array[index + 1];
+            return currentValue - next
+        });
+
+        const hasSequentialRanks = this.getUniqueValues(differencesArray).length === 1;
+        if (hasSimilarSuits && hasSequentialRanks) {
+            this.setState({text: 'Straight flush!!!'});
+            return
+        }
+
+        const countRanks = this.countSimilars(ranks);
+        const countRanksValues = Object.values(countRanks);
+        const has4similarRanks = countRanksValues.includes(4);
+        if (has4similarRanks) {
+            this.setState({text: 'Four of a kind!!!'});
+            return
+        }
+
+        const has3similarRanks = countRanksValues.includes(3);
+        const has2similarRanks = countRanksValues.includes(2);
+
+        if (has3similarRanks && has2similarRanks) {
+            this.setState({text: 'Full house!!!'});
+            return
+        }
+
+        if (hasSimilarSuits) {
+            this.setState({text: 'Flush!!!'})
+        }
+
+        if (has3similarRanks) {
+            this.setState({text: 'Three of a kind!!!'});
+            return
+        }
+
+        const hasTwoPairs = countRanksValues.length === 3 && this.getUniqueValues(countRanksValues).length === 2;
+
+        if (hasTwoPairs) {
+            this.setState({text: 'Two pairs!!!'});
+            return
+        }
+
+        if (has2similarRanks) {
+            this.setState({text: 'One pair!!!'});
+            return
+        }
+
+        this.setState({text: 'Sorry, try again!!!'});
     };
 
 
